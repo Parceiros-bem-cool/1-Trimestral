272e6dbded292717649dc891eabd05f73ba84eeb
vinicudin <99336316+vinicudin@users.noreply.github.com>
2022-04-20 23:13:05 -0300

    Create codigo.ino

---
+++ codigo.ino
@@ -0,0 +1,337 @@
+int pin_a = 4;
+int pin_b = 5;
+int pin_c = 1;
+int pin_d = 7;
+int pin_e = 6;
+int pin_f = 3;
+int pin_g = 2;
+
+void setup()
+{
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(pin_e, OUTPUT);
+ pinMode(pin_c, OUTPUT);
+ pinMode(pin_b, OUTPUT);
+ pinMode(pin_g, OUTPUT);
+ pinMode(pin_f, OUTPUT);
+ pinMode(pin_a, OUTPUT);
+ pinMode(pin_d, OUTPUT);
+
+}
+  void loop ()
+  {
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+
+
+
+
+
+digitalWrite(13, LOW);
+digitalWrite(8, HIGH);
+    
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_e, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_d, LOW);
+digitalWrite(pin_f, LOW);
+digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+digitalWrite(8, LOW);
+ digitalWrite(11, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_e, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_d, LOW);
+digitalWrite(pin_f, LOW);
+digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+digitalWrite(13, HIGH);
+digitalWrite(11, LOW);
+  delay(3000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_e, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_d, LOW);
+digitalWrite(pin_f, LOW);
+digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+digitalWrite(9, HIGH);
+digitalWrite(12, LOW);
+delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_e, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_d, LOW);
+digitalWrite(pin_f, LOW);
+digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+digitalWrite(10, HIGH);
+ digitalWrite(9, LOW);
+    delay(3000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_e, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_d, LOW);
+digitalWrite(pin_f, LOW);
+digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+digitalWrite(10, LOW);
+    digitalWrite(12, HIGH);
+delay(3000); 
+ digitalWrite(pin_e, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_d, LOW);
+digitalWrite(pin_f, LOW);
+digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_d, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_c, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_d, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_g, HIGH);
+  digitalWrite(pin_e, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin_d, LOW);
+  digitalWrite(pin_a, LOW);
+  digitalWrite(pin_b, LOW);
+  digitalWrite(pin_g, LOW);
+  digitalWrite(pin_e, LOW);
+  
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(pin_b, LOW);
+  digitalWrite(pin_c, LOW);
+digitalWrite(pin_e, HIGH);
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+  digitalWrite(pin_c, HIGH);
+  digitalWrite(pin_d, HIGH);
+digitalWrite(pin_f, HIGH);
+ delay(1000); // Wait for 1000 millisecond(s)
+ pinMode(pin_e, LOW);
+ pinMode(pin_c, LOW);
+ pinMode(pin_b, LOW);
+ pinMode(pin_g, LOW);
+ pinMode(pin_f, LOW);
+ pinMode(pin_a, LOW);
+ pinMode(pin_d, LOW);
+  
+}
